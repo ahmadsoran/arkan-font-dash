8a6752e6f639c9ec01c5b35da4e92f8a5dadd643
ahmadsoran <ahmadsorann@GMAIL.COM>
2022-06-16 05:42:07 +0300

    loading route err fix on ios

--- src/routes.js
+++ src/routes.js
@@ -8,23 +8,21 @@ import User from './pages/User';
 import Login from './pages/Login';
 import NotFound from './pages/Page404';
 import DashboardApp from './pages/DashboardApp';
-import { useProfileQuery } from './app/appApi';
 import UploadFonts from './components/UploadFonts';
 
 // ----------------------------------------------------------------------
 
 export default function Router() {
-  const { data } = useProfileQuery()
   return useRoutes([
     {
 
       path: '/dashboard',
       element: <DashboardLayout />,
       children: [
-        { path: 'app', element: data && <DashboardApp /> },
-        { path: 'user', element: data && <User /> },
-        { path: 'uploadFonts', element: data && <UploadFonts /> },
-        { path: 'fonts', element: data && <Blog /> },
+        { path: 'app', element: <DashboardApp /> },
+        { path: 'user', element: <User /> },
+        { path: 'uploadFonts', element: <UploadFonts /> },
+        { path: 'fonts', element: <Blog /> },
       ],
     },
     {
