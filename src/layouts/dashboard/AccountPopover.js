7b0c81e98db8bb7e6e62f3e3e2987b99abe7cab0
ahmadsoran <ahmadsorann@GMAIL.COM>
2022-06-19 04:23:33 +0300

    remove settings & profile option

--- src/layouts/dashboard/AccountPopover.js
+++ src/layouts/dashboard/AccountPopover.js
@@ -18,17 +18,7 @@ const MENU_OPTIONS = [
     label: 'Home',
     icon: 'eva:home-fill',
     linkTo: '/',
-  },
-  {
-    label: 'Profile',
-    icon: 'eva:person-fill',
-    linkTo: '#',
-  },
-  {
-    label: 'Settings',
-    icon: 'eva:settings-2-fill',
-    linkTo: '#',
-  },
+  }
 ];
 
 // ----------------------------------------------------------------------
